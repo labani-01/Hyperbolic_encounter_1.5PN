39a65505cc390778a710e224ed9098ddedb9aaa5
Kanchan Soni <kanchansoni@Mac.fios-router.home>
2025-11-05 15:02:46 -0500

    modified files; cleaned up codes

--- Hyperbolic15PNhphc.py
+++ Hyperbolic15PNhphc.py
@@ -1,18 +1,14 @@
 import hyperbolic_waveform_generate
 import numpy as np
-from scipy.integrate import solve_ivp
-from matplotlib import pyplot as plt
 pi = np.pi
 from pycbc.waveform import get_td_waveform
-from pycbc.waveform import td_approximants, fd_approximants
-import pycbc.conversions
 from pycbc.types import TimeSeries
 import pycbc.coordinates as cord
+from pycbc import pnutils
 
 def hyperbolic_waveform_td(**kwds):
     m1 = kwds['mass1']
     m2 = kwds['mass2']
-    dt = kwds['delta_t']
     R = kwds['distance']
     Theta = kwds['inclination']
     spin1_a, spin1_polar, spin1_azimuthal = cord.cartesian_to_spherical(kwds['spin1x'], kwds['spin1y'], kwds['spin1z'])
@@ -40,39 +36,24 @@ def hyperbolic_waveform_td(**kwds):
     hp, hc = hyperbolic_waveform_generate.hphc_15PN(vmax, duration, chi1, \
                     theta1i, phi1i, chi2, theta2i, phi2i, \
                     m1, m2, et0, R, Theta, delta_t, phi0)
-
     hp = TimeSeries(hp, delta_t)
     hc = TimeSeries(hc, delta_t)
-
-    # t = hp.sample_times - hp.sample_times[np.argmax(hp)+1]
-    #hp = TimeSeries(hp, delta_t, epoch=min(t))
-    #hc = TimeSeries(hc, delta_t, epoch=min(t))
-
     # Find the peak time and shift the time axis
     t_shift = hp.sample_times[np.argmax(hp)]
 
     hp = TimeSeries(hp.data, delta_t, epoch=-t_shift)
     hc = TimeSeries(hc.data, delta_t, epoch=-t_shift)
-
     return hp, hc
 
 
 def hyperbolic_waveform_fd(**kwds):
-    from pycbc.waveform import get_td_waveform
-    from pycbc.waveform.utils import apply_fseries_time_shift
-    from pycbc.waveform import td_approximants, fd_approximants
-    from pycbc.waveform import utils as wfutils
-    import numpy as np
     if 'approximant' in kwds:
         kwds.pop('approximant')
-    #hp, hc = get_td_waveform(approximant="Hyperbolic15PNhphc", **kwds)
 
     kwds.update({
         "approximant": "Hyperbolic15PNhphc",
         })
     nparams = kwds.copy()
-
-    #full_duration = duration = len(hp)*hp.delta_t
 
     if 'f_fref' not in nparams:
         nparams['f_ref'] = kwds['f_lower']
@@ -87,19 +68,14 @@ def hyperbolic_waveform_fd(**kwds):
         delta_t = 1.0 / 2048
     nparams['delta_t'] = delta_t
     hp, hc = get_td_waveform(**nparams)
+
     # Resize to the right duration
     tsamples = int(1.0 / kwds['delta_f'] / delta_t)
     if tsamples < len(hp):
         raise ValueError("The frequency spacing (df = {}) is too low to "
                          "generate the {} approximant from the time "
-                         "domain".format(params['delta_f'], params['approximant']))
+                         "domain".format(kwds['delta_f'], kwds['approximant']))
 
-    # apply the tapering, we will use a safety factor here to allow for
-    # somewhat innacurate duration difference estimation.
-    #window = (full_duration - duration) * 0.8
-    #hp = wfutils.td_taper(hp, hp.start_time, hp.start_time + window)
-    #hc = wfutils.td_taper(hc, hc.start_time, hc.start_time + window)
-    # avoid wraparound
     hp = hp.to_frequencyseries().cyclic_time_shift(hp.start_time)
     hc = hc.to_frequencyseries().cyclic_time_shift(hc.start_time)
     return hp, hc
--- setup.py
+++ setup.py
@@ -2,29 +2,23 @@
 setup.py file for line of sight acceleration parameter pycbc waveform plugin package
 """
 
-from setuptools import Extension, setup, Command
-from setuptools import find_packages
+from setuptools import setup
 
-VERSION = '0.0.dev0'
-
-
-
-
-
+VERSION = '1.0.dev0'
 
 setup (
     name = 'pycbc-Hyperbolic15PNhphc',
     version = VERSION,
     description = 'New waveform to generate hyperbolic encounter plugin PyCBC',
     long_description = open('descr.rst').read(),
-    author = 'The PyCBC team',
-    author_email = 'labannyo2000@gmail.org',
+    author = 'Labani Roy, Kanchan Soni',
+    author_email = 'lroy02@syr.edu, ksoni01@syr.edu',
     url = 'http://www.pycbc.org/',
     download_url = 'https://github.com/labani-01/Hyperbolic_encounter_1.5PN',
     keywords = ['pycbc', 'signal processing', 'gravitational waves'],
     install_requires = ['pycbc'],
     py_modules = ['hyperbolic_waveform_generate','Hyperbolic15PNhphc'],
-    entry_points = {"pycbc.waveform.td":"Hyperbolic15PNhphc = Hyperbolic15PNhphc:hyperbolic_waveform_td", 
+    entry_points = {"pycbc.waveform.td":"Hyperbolic15PNhphc = Hyperbolic15PNhphc:hyperbolic_waveform_td",
                     "pycbc.waveform.fd": "Hyperbolic15PNhphc = Hyperbolic15PNhphc:hyperbolic_waveform_fd"},
     classifiers=[
         'Programming Language :: Python',
@@ -38,4 +32,5 @@ setup (
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     ],
 )
+
 
